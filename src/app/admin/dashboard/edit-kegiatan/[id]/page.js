113a094d66467ec5ec84e468e1ad9dd11e9b39d6
riskihamdani24 <145739987+riskihamdani24@users.noreply.github.com>
2026-09-02 01:12:45 +0700

    Delete src directory

--- src/app/admin/dashboard/edit-kegiatan/[id]/page.js
+++
@@ -1,6 +0,0 @@
-import EditKegiatanView from './EditKegiatanView';
-
-export default async function EditKegiatanPage({ params }) {
-  const { id } = await params;
-  return <EditKegiatanView activityId={id} />;
-}
